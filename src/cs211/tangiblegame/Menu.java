56d84c6f22a83fd3afe8a83e64055826bd258ffa
Maxime Hulliger <maxime.hulliger@epfl.ch>
2015-06-14 23:58:11 +0200

    more stable

--- src/cs211/tangiblegame/Menu.java
+++ src/cs211/tangiblegame/Menu.java
@@ -7,19 +7,19 @@ public class Menu extends Interface {
 	}
 
 	public void draw() {
+		app.background(103);
 		app.fill(0);                       
 		app.text("1: Real game", 200 ,200);
 		app.text("2: Trivial game", 200 ,250);
 		app.text("3: Calibration", 200 ,300);
-			
 	}
 	
 	public void keyPressed() {
-		if (app.key == '1') {
+		if (app.key == '1')
 			app.setInterface(app.intRealGame);
+		if (app.key == '2')
 			app.setInterface(app.intTrivialGame);
+		if (app.key == '3')
 			app.setInterface(app.intCalibration);
-		}
 	}
-
 }
