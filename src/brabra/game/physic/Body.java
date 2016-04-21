685af1e4fd82299498b20659d9961bb0ed67c7ce
Maxime Hulliger <maxime.hulliger@epfl.ch>
2016-04-21 17:33:14 +0200

    hot files

---
+++ src/brabra/game/physic/Body.java
@@ -0,0 +1,365 @@
+package brabra.game.physic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import brabra.game.Color;
+import brabra.game.Observable.NVector;
+import brabra.game.XMLLoader.Attributes;
+import brabra.game.physic.geo.Line;
+import brabra.game.physic.geo.Quaternion;
+import brabra.game.physic.geo.Vector;
+import brabra.game.scene.Movable;
+import brabra.game.scene.Object;
+
+/** 
+ * An movable object obeying to the laws of physics. 
+ * It has a mass and a moment of inertia (angular mass). 
+ * You can apply forces and impulse (interactions) to it to move it.
+ * TODO: If has a parent, apply it to the parent.
+ **/
+public abstract class Body extends Movable {
+	
+	public static final float infiniteMass = -1, ghostMass = -2; //TODO change all -1 & -2
+	private static final boolean displayInteractions = true; //forces & impulse
+	private static final Color interactionColor = new Color("white", true);
+	
+	/** Mass of the body. -2 for ghost, -1 for infinite or bigger than 0 (never equals 0).*/
+	protected float mass = ghostMass;
+	/** Inverse of the body mass. 0 for infinite mass or bigger than 0. */
+	protected float inverseMass = 0;
+	protected Vector inertiaMom = null;
+	protected Vector inverseInertiaMom = null;
+	protected Color color = Color.basic;
+	/** Coefficient of restitution in [0, 1] for stable simulation. */
+	protected float restitution = 0.8f;
+	protected int life = -1;
+	protected int maxLife = -1;
+	
+	/** Will be added on next update. */
+	private NVector forcesLocToAdd = new NVector(), torquesLocToAdd = new NVector();
+	/** Executed before updating the body. */
+	private Consumer<Body> onUpdate = null;
+	private List<Line> interactionsRel = new ArrayList<>();
+	
+	
+	/** create a Body with this location & location and infinite mass. rotation can be null */
+	public Body(Vector location, Quaternion rotation) {
+		super(location, rotation);
+	}
+	
+	public void copy(Object o) {
+		super.copy(o);
+		Body ob = this.as(Body.class);
+		if (ob != null) {
+			setMass(ob.mass);
+			onUpdate = ob.onUpdate;
+			maxLife = ob.maxLife;
+			life = ob.life;
+			color = ob.color;
+			restitution = ob.restitution;
+		}
+	}
+
+	/** to react to a collision */
+	protected void onCollision(Collider col, Vector impact) {}
+
+	public boolean validate(Attributes atts) {
+		if (super.validate(atts)) {
+			final String color = atts.getValue("color");
+			if (color != null)
+				setColor( new Color(color, atts.getValue("stroke")) );
+			final String mass = atts.getValue("mass");
+			if (mass != null)
+				setMass(Float.parseFloat(mass));
+			final String life = atts.getValue("life");
+			if (life != null)
+				setLife(life);
+			final String impulse = atts.getValue("impulse");
+			if (impulse != null)
+				applyImpulse(vec(impulse));
+			return true;
+		} else
+			return false;
+	}
+	
+	/** applique les forces et update l'etat. return true if this was updated. */
+	public boolean update() {
+		if (!updated) {
+			// before update
+			if (onUpdate != null)
+//<<<<<<< HEAD
+				onUpdate.accept(this);
+			
+			// apply if needed
+			final boolean touched = forcesLocToAdd.hasChangedCurrent() || torquesLocToAdd.hasChangedCurrent();
+			if (inverseMass > 0 && touched) {
+				// translation
+				Vector acceleration = forcesLocToAdd.multBy(inverseMass);
+				if (!acceleration.equals(zero))
+					velocityRel.add( acceleration );
+				// rotation, vitesse angulaire, on prend rotation axis comme L/I
+				Vector dL = torquesLocToAdd.multElementsBy(inverseInertiaMom);
+				if (!dL.equals(zero))
+					//rotationRelVel.addAngularMomentum( dL );
+					addAngularMomentum( dL );
+			}
+			
+			// reset
+			if (touched) {
+				forcesLocToAdd.reset(zero);
+				torquesLocToAdd.reset(zero);
+			} 
+//=======
+//				onUpdate.run();
+//			//1. translation
+//			Vector acceleration = forcesLocToAdd.multBy(inverseMass);
+//			if (!acceleration.equals(zero))
+//				velocityRel.add( acceleration );
+//			//2. rotation, vitesse angulaire, on prend rotation axis comme L/I
+//			Vector dL = torquesLocToAdd.multElementsBy(inverseInertiaMom);
+//			if (!dL.equals(zero))
+//				addAngularMomentum( dL );
+//
+//			forcesLocToAdd.set(zero);
+//			torquesLocToAdd.set(zero);
+//>>>>>>> refs/remotes/origin/master
+			return super.update(); //always true
+		} else
+			return false;
+	}
+	
+	/** Display the interactions... maybe. In local space (should be call after pushLocal()). */
+	protected void displayInteractionMaybe() {
+		if (displayInteractions) {
+			for (Line l : interactionsRel)
+				l.display(interactionColor);
+			interactionsRel.clear();
+		}
+	}
+	
+	public void onDelete() {
+		game.debug.log(6, this+" deleted.");
+		super.onDelete();
+	}
+
+	// --- some setters
+	
+	/** To execute some code before this body is updated (for example to apply some forces). */
+	public void addOnUpdate(Consumer<Body> onUpdate) {
+		final Consumer<Body> oldAddForce = this.onUpdate;
+		this.onUpdate = this.onUpdate == null ?
+				onUpdate : b -> { oldAddForce.accept(b); onUpdate.accept(b); };
+	}
+
+	/** 
+	 * 	Set the body mass. 
+	 * 	If mass is -1, the body will have an infinite mass and inertia moment 
+	 * 	and won't be affected by collisions (but others will).
+	 * 	If mass is 0, the body becomes a ghost (with infinite mass -> same as -1) 
+	 * 	and don't react to physic (nor with others).
+	 *	Should be overload (and called) to set the inertia moment (depending on the shape). 
+	 **/
+	public void setMass(float mass) {
+		if (mass < -1) {
+			game.debug.err("invalide mass: "+mass+", taking 0 (ghost)");
+			setMass(0);
+		} else if (mass == -1) {
+			this.mass = -1;
+			this.inverseMass = 0;
+			this.inertiaMom = Vector.cube(Float.POSITIVE_INFINITY);
+			this.inverseInertiaMom = zero.copy();
+		} else if (mass == 0) {
+			setMass(-1);
+			this.mass = -2; //-> ghost
+		} else { //mass > 0
+			this.mass = mass;
+			this.inverseMass = 1/mass;
+			// to be set by concrete class
+			this.inertiaMom = null;
+			this.inverseInertiaMom = null;
+		}
+		model.notifyChange(Change.Mass);
+	}
+	
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	// --- life
+	
+	/** to react to the death from damages */
+	protected void onDeath() {}
+	
+	public void setLife(int life, int maxLife) {
+		if (maxLife <= 0) {
+			game.debug.err(toString()+" should have a positive maxLife, set to 1 (0 is dead).");
+			maxLife = 1;
+		}
+		if (life < 0) {
+			game.debug.err(toString()+" should have a positive life, killing him...");
+			life = 0;
+			onDeath();
+		}
+			
+		this.maxLife = maxLife;
+		if (life > maxLife) {
+			this.life = maxLife;
+			game.debug.err(toString()+" can not have more life than maxLife, setting at "+life());
+		} else {
+			this.life = life;
+		}
+	}
+
+	private void setLife(String lifeText) {
+		final String[] sub = lifeText.split("/");
+		if (sub.length == 2)
+			setLife(Integer.parseInt(sub[0]),Integer.parseInt(sub[1]));
+		else if (sub.length == 1) {
+			int life = Integer.parseInt(sub[0]);
+			setLife(life, life);
+		} else
+			game.debug.err("unsuported life format: \""+lifeText+"\". letting to "+life());
+	}
+
+	public void damage(int damage) {
+		if (maxLife < 0)
+			game.debug.log(4, this+" is a poor non-living object !");
+		else if (life == 0)
+			game.debug.log(5, this+" is already dead !");
+		else {
+			life -= damage;
+			if (life <= 0 ) {
+				life = 0;
+				onDeath();
+			}
+		}
+	}
+	
+	public String life() {
+		return life+"/"+maxLife;
+	}
+
+	// --- Physic management:  impulse, forces and torques ---
+
+	/** If true, the body doesn't interact with others (and others don't). */
+	public boolean ghost() {
+		return mass < -1;
+	}
+	
+	/** If false, the body doesn't react to the collision but others do. */
+	public boolean affectedByCollision() {
+		return inverseMass > 0;
+	}
+	
+	/** Return the mass of the body, bigger than 0 (can be Float.POSITIVE_INFINITY). */
+	public float mass() {
+		return inverseMass > 0 ? mass : Float.POSITIVE_INFINITY;
+	}
+
+	/** Add momentum to the body at this point (absolute). */
+	public void applyImpulse(Vector posAbs, Vector impulseAbs) {
+		assert(!impulseAbs.equals(zero));
+		// in local space
+		Vector posLoc = local(posAbs);
+		Vector impulseLoc = localDir(impulseAbs);
+		interactionsRel.add(new Line(relative(posAbs), relative(add(posAbs, impulseAbs.multBy(4))), true));		
+		// test if the impulse is against (or on) the mass center -> just translation
+		if (posLoc.isZeroEps(false) || posLoc.cross(impulseLoc).isZeroEps(false)) {
+			velocityRel.add( impulseLoc.multBy(this.inverseMass) );
+		} else {
+			Vector posLocN = posLoc.normalized();
+			// for translation, just in absolute
+			Vector dVel = posLocN.multBy(impulseLoc.dot(posLocN)*this.inverseMass);
+			if (!dVel.equals(zero))
+				velocityRel.add( dVel );
+			// for rotation, we want to stay coherent with inertia moment.
+			Vector dL = inverseInertiaMom.multElementsBy(posLoc.cross(impulseLoc));
+			if (!dL.equals(zero))
+				addAngularMomentum( dL );
+		}
+	}
+	
+	/** Add linear momentum to the body (absolute). */
+	public void applyImpulse(Vector impulseAbs) {
+		applyImpulse(location(), impulseAbs);
+	}
+
+	/** Apply a force on the body at this point (absolu). */
+	public void applyForce(Vector posAbs, Vector forceAbs) {
+		applyForceLoc(local(posAbs), localDir(forceAbs));
+	}
+	
+	/** Apply a force on the body at this point (in this object's space). */
+	public void applyForceRel(Vector posRel, Vector forceRel) {
+		applyForceLoc(localFromRel(posRel), localDirFromRel(forceRel));
+	}
+
+	/** Add a pure translation force on the body at the mass center (from absolute). */
+	public void applyForce(Vector forceAbs) {
+		applyForceLoc(localDir(forceAbs));
+	}
+
+	/** Add a pure translation force on the body at the mass center (from relative). */
+	public void applyForceRel(Vector forceRel) {
+		applyForceLoc(localDirFromRel(forceRel));
+	}
+
+	/** Main methods to add a force. we work in local space. */
+	private void applyForceLoc(Vector posLoc, Vector forceLoc) {
+		assert(!forceLoc.equals(zero));
+		interactionsRel.add(new Line(relativeFromLocal(posLoc), relativeFromLocal(add(posLoc, forceLoc.multBy(4))), true));
+		if (posLoc.isZeroEps(false) || posLoc.cross(forceLoc).isZeroEps(false)) {
+			// if the force is against (or on) the mass center -> just translation
+			applyForceLoc( forceLoc );
+		} else {
+			Vector posLocN = posLoc.normalized();
+			// for translation, just in absolute
+			float forceFactor = forceLoc.dot(posLocN);
+			if (forceFactor != 0)
+				applyForceLoc( posLocN.multBy(forceFactor) );
+			// for rotation, we want to stay coherent with inertia moment.
+			applyTorqueLoc( posLoc.cross(forceLoc) );
+		}
+	}
+
+	/** add a pure translation force on the body at the mass center (from parent space). */
+	private void applyForceLoc(Vector forceLoc) {
+		assert(!forceLoc.equals(zero));
+		if (!forceLoc.equals(zero))
+			forcesLocToAdd.add(forceLoc);
+	}
+
+	/** add a torque (rotational force) on the body (from parent space). */
+	private void applyTorqueLoc(Vector torqueLoc) {
+		assert(!torqueLoc.equals(zero));
+		if (!torqueLoc.equals(zero))
+			torquesLocToAdd.add(torqueLoc);
+	}
+	
+	private void addAngularMomentum(Vector dL) {
+		assert (!dL.equals(zero));
+		if (rotationRelVel.isIdentity())
+			rotationRelVel.set(dL, dL.mag());
+		else {
+			final Vector newRotAxis = rotationRelVel.rotAxisAngle().plus(dL);
+			rotationRelVel.set(newRotAxis, newRotAxis.mag());
+		}
+	}
+	
+	// --- cooked methods to apply forces
+	
+	/** apply his weight to the object. */
+	public void pese() {
+		if (mass == -1)
+			//throw new IllegalArgumentException("un objet de mass infini ne devrait pas peser !"); now tolerated :)
+			return;
+		applyForce( app.para.gravity().multBy(mass) );
+	}
+	
+	public void avance(float forceFront) {
+		assert(forceFront != 0);
+		applyForceRel( front(forceFront) );
+	}
+}
---
+++ src/brabra/game/physic/geo/Quaternion.java
@@ -0,0 +1,434 @@
+package brabra.game.physic.geo;
+
+import brabra.Master;
+import brabra.ProMaster;
+import processing.core.PApplet;
+import brabra.game.physic.Physic;
+import brabra.game.physic.geo.Vector;
+import brabra.game.Observable.NQuaternion;
+
+public class Quaternion extends ProMaster {
+	
+	public static final float 	epsilonRotAxis = 1E-7f,
+								epsilonAngle = 1E-6f,//pi/(360*1000),
+								epsilonWXYZ = 1E-6f;
+	public static final Quaternion identity = new NQuaternion(new Quaternion(), 
+			() -> { throw new IllegalArgumentException("pas touche à l'identity /!\\"); });
+
+	// components of a quaternion
+	private float w = 1;  
+	private final Vector xyz = zero.copy();
+	
+	// lazily updated component when rotated, rotAxis always normalized or null (if valid)
+	private boolean validRotAxis = true;
+	private Vector rotAxis = null;	
+	private float angle = 0;
+
+	
+	// --- Builders ---
+
+	/** Create a new null (identity) Quaternion from wxyz. */
+	public Quaternion() {
+		// everything is already set :)
+	}
+	
+	/** Create a new Quaternion from wxyz. */
+	public Quaternion(float w, float x, float y, float z) {
+		this(w, new Vector(x,y,z));
+	}
+
+	/** Create a new Quaternion from angle and axis. */
+	public Quaternion(Vector axis, float angle) {
+		set(axis == null ? null : axis.copy(), angle);
+	}
+	
+	/** Create a copy of the Quaternion. */
+	public Quaternion(Quaternion q) {
+		set(q);
+	}
+	
+	public Quaternion (float w, Vector xyz) {
+		set(w, xyz);
+	}
+
+	// --- Getters ---
+	
+	/** return the rot axis or null if identity. */
+	public Vector rotAxis() {
+		updateAxis();
+		return rotAxis == null ? null : rotAxis.copy();
+	}
+	
+	/** return the rot axis or null if identity. */
+	public Vector rotAxisAngle() {
+		updateAxis();
+		return rotAxis == null ? null : rotAxis.multBy(angle);
+	}
+	
+	/** Return the angle of rotation in radians. */
+	public float angle() {
+		updateAxis();
+		return angle;
+	}
+	
+	public boolean isIdentity() {
+		return validRotAxis && rotAxis == null ? true : w == 1 || w == -1;
+	}
+	
+	private boolean testing = false;
+	/** Collection of assert to ensure a valid quat. */
+	private void testThis() {
+		if (!testing) {
+			testing = true;
+			final boolean id = isIdentity();
+			if (id) {
+				if (validRotAxis) {
+					assert (rotAxis==null);
+					assert (angle == 0);
+				}
+				assert (w == 1 || w == -1);
+				assert (xyz.equals(zero));
+			} else {
+				if (validRotAxis) {
+					assert (rotAxis!=null);
+					assert (angle != 0);
+				}
+				assert (w != 1 && w != -1);
+				assert (!xyz.equals(zero));
+			}
+			
+			testing = false;
+		}
+	}
+	
+	public boolean equals(Object other) {
+		if (other == null) return false;
+	    if (other == this) return true;
+	    return (other instanceof Quaternion) 
+	    		? equals((Quaternion)other) : false;
+	}
+
+	public boolean equals(Quaternion other) {
+		return w == other.w && xyz.equals(other.xyz);
+	}
+
+	public boolean equalsAxis(Quaternion other) {
+		return rotAxis() == other.rotAxis() && angle() == other.angle();
+	}
+	
+	public static boolean printEverything = true;
+	
+	public String toString() {
+		if (app != null && app.debug.testMode) 
+			return toStringDebug();
+		else
+			return isIdentity() ? "Identity" : "around: "+rotAxis()+" with angle "+angle()*toDegrees+"°";
+	}
+	
+	public String toStringDebug() {
+		return    "quat: (wxyz:"+w+", "+xyz.x+", "+xyz.y+", "+xyz.z+")"
+				+ (validRotAxis 
+					? " \naround "+rotAxis()+" with angle "+(angle()*toDegrees)+"°"
+					: " with unknown axis");
+	}
+	
+	// --- Setters ---
+	
+	/** Set the quaternion from WXYZ, normalize this, unvalidate rot axis. */
+	public void set(float w, Vector xyz) {
+		setWXYZ(w, xyz, true);
+		checkIdentity();
+	}
+	
+	/** Set the quaternion from rotation axis & angle and return it. */
+	public void set(Vector rotAxis, float angle) {
+		angle = mod(angle, twoPi);
+		assert(isConstrained(angle, 0, twoPi));
+		
+		if (rotAxis == null || angle == 0)
+			reset();
+		else {
+			assert !(rotAxis.equals(zero));
+			rotAxis.normalize();
+			
+			final float halfOmega = angle/2; 
+			setWXYZ(PApplet.cos(halfOmega), rotAxis.multBy(PApplet.sin(halfOmega)), true);
+			if (!checkIdentity()) {
+				//TODO: Mmmh.. why does commenting the next line change that much the test results ?
+				setRotAxisAngle(rotAxis, angle, false);
+			}
+		}
+		testThis();
+	}
+	
+	public String formated() {
+		//TODO not good epsilon
+		return rotAxis()==null ? "identity" 
+				: "around "+rotAxis().formated(Physic.epsilon)+" with angle "+Master.formatFloat(angle()*180/pi, Physic.epsilon)+"°";
+	}
+
+	/** wxyz -> (rotAxis, angle). update rot axis & angle if needed (!validRotAxis). set validRotAxis to true. */
+	private void updateAxis() {
+		if (!validRotAxis) {
+			if (isIdentity()) {
+				reset();
+			} else {
+				//more stable than halfomega = PApplet.acos(w);
+				final float halfOmega = PApplet.acos(w);//atan2(xyz.mag(), w);
+				final float s = PApplet.sin(halfOmega);
+				if (halfOmega == 0 || s == 0) { //no rotation
+					reset();
+				} else {
+					final Vector rotAxis = xyz.multBy(1/s);
+					if (rotAxis.equals(Vector.zero))
+						reset();
+					else
+						setRotAxisAngle(rotAxis, 2*halfOmega, true);
+				}
+			}
+		}
+		testThis();
+	}
+
+	/** Set the quaternion from another quaternion (deep copy). */
+	public void set(Quaternion quat) {
+		// we trust that the quat is valid.
+		setWXYZ(quat.w, quat.xyz, false);
+		testThis();
+		if (quat.validRotAxis) 
+			setRotAxisAngle(quat.rotAxis, quat.angle, false);
+		testThis();
+	}
+	
+	public void setAngle(float angle) {
+		set(rotAxis(), angle);
+	}
+	
+	/** Mult/turn the quaternion by r and return it. */
+	public void rotate(Quaternion r) {
+		if (!r.isIdentity()) {
+			if (isIdentity())
+				set(r);
+			else {
+				set(r.w*w - r.xyz.x*xyz.x - r.xyz.y*xyz.y - r.xyz.z*xyz.z,
+						new Vector(
+						r.w*xyz.x + r.xyz.x*w - r.xyz.y*xyz.z + r.xyz.z*xyz.y,
+						r.w*xyz.y + r.xyz.y*w - r.xyz.z*xyz.x + r.xyz.x*xyz.z,
+						r.w*xyz.z + r.xyz.z*w - r.xyz.x*xyz.y + r.xyz.y*xyz.x)
+					);
+			}
+		}
+		testThis();
+	}
+
+	// --- Other creators ---
+
+	public Quaternion copy() {
+		return new Quaternion(this);
+	}
+	
+	/** return the equivalent opposite quaternion (minus rotAxis & minus angle is the same rotation). */
+	public Quaternion contrary() {
+		Quaternion q = new Quaternion(-w, xyz.multBy(-1));
+		if (isIdentity())
+			q.setRotAxisAngle(null, 0, false);
+		else
+			q.setRotAxisAngle(rotAxis().multBy(-1), -angle, false);
+		return q;
+	}
+
+	public Quaternion rotatedBy(Quaternion r) {
+		final Quaternion q = copy();
+		q.rotate(r);
+		return q;
+	}
+
+	public Quaternion withAngle(float angle) {
+		final Quaternion q = copy();
+		q.setAngle(angle);
+		return q;
+	}
+
+	public Quaternion withOppositeAngle() {
+		return new Quaternion(w, xyz.multBy(-1));
+	}
+	
+	// --- Static Builder ---
+	
+	/** Retutn a new Quaternion representing a quaternion from 2 direction vectors and return it. Forward and upwards shouldn't be colinear. */
+	public static Quaternion fromDirection(Vector forward, Vector upwards) {
+		forward = forward.normalized();
+		upwards = upwards.normalized();
+		if (forward.equalsEps(front, false))		// front
+    		return identity.copy();
+		else if (forward.equalsEps(behind, false))	// behind
+    		return new Quaternion(Vector.up, pi);
+		else if (forward.equalsEps(up, false))		// up
+			return new Quaternion(left, PApplet.PI/2);
+		else if (forward.equalsEps(down, false))	// down
+			return new Quaternion(left, -PApplet.PI/2);
+		else {
+			final Vector right = upwards.cross(forward).normalized();    
+			assert(!right.equals(zero));
+			final Vector up = forward.cross(right).normalized();
+			assert(!up.equals(zero));
+			final float in = 1.0f + right.x + up.y + forward.z;
+	        if (in == 0) {
+	        	return identity.copy();
+	        } else {
+	        	assert (in > 0);
+				final float w = PApplet.sqrt(in) / 2.0f;
+		        final float dfwScale = w * 4.0f;
+		        return new Quaternion(w, (forward.y - up.z) / dfwScale,
+		        		(right.z - forward.x) / dfwScale, (up.x - right.y) / dfwScale);
+	        }
+		}
+    }
+
+	/** Return a new Quaternion with a rotation between a and b with factor t[0,1]. */
+	public static Quaternion slerp(Quaternion a, Quaternion b, float t) {
+		final float omega, cosom, sinom, scla, sclb;
+		cosom = a.xyz.x*b.xyz.x + a.xyz.y*b.xyz.y + a.xyz.z*b.xyz.z + a.w*b.w;
+		if ((1.0f+cosom) > Float.MIN_VALUE) {
+			if ((1.0f-cosom) > Float.MIN_VALUE) {
+				omega = PApplet.acos(cosom);
+				sinom = PApplet.sin(omega);
+				scla = PApplet.sin((1.0f-t)*omega) / sinom;
+				sclb = PApplet.sin(t*omega) / sinom;
+			} else {
+				scla = 1.0f - t;
+				sclb = t;
+			}
+			return new Quaternion( scla*a.w + sclb*b.w, a.xyz.multBy(scla).plus(b.xyz.multBy(sclb)) );
+		} else {
+			scla = PApplet.sin((1f-t) * PApplet.PI * 0.5f);
+			sclb = PApplet.sin(t * PApplet.PI * 0.5f);
+			return new Quaternion (a.w, a.xyz.multBy(scla).plus(b.xyz.multBy(sclb)));
+		}
+	}
+	
+	// --- Equal Epsilon ---
+
+	/** Check if the quaternion is nearly null. If so & clean, reset it. */
+	public boolean isZeroEps(boolean clean) {
+		return equalsEps(identity, clean);
+	}
+
+	/** Check if the quaternion is nearly equal to the other. If so & clean, set this to other. */
+	public boolean equalsEps(Quaternion other, boolean clean) {
+		return equalsEpsWXYZ(other, clean);
+	}
+
+	/** Check if this (wxyz) nearly equals other and if so & clean set it to other. */
+	public boolean equalsEpsWXYZ(Quaternion other, boolean clean) {
+		if ((Physic.equalsEps(w, other.w, epsilonWXYZ) && xyz.equalsEps(other.xyz, false, epsilonWXYZ))
+				|| (Physic.equalsEps(w, -other.w, epsilonWXYZ) && xyz.equalsEps(other.xyz.multBy(-1), false, epsilonWXYZ))) {
+			if (clean && !equals(other))
+				set(other);
+			return true;
+		} else
+			return false;
+	}
+
+	/** Check if this (axis & angle) nearly equals other and if so & clean set it to other. */
+	public boolean equalsEpsAxis(Quaternion other, boolean clean) {
+		final Vector rMe = rotAxis(), rOther = other.rotAxis();
+		final float aMe = angle(), aOther = other.angle(), aOtherMinus = -aOther;
+		final boolean aEqu = anglesEquEps(aMe, aOther), aEquMinus = anglesEquEps(aMe, aOtherMinus);
+		if (rMe == null) {
+			if (aEqu && clean && !equals(other))
+				set(other);
+			return aEqu;
+		} else if ((rMe.equalsEps(rOther, false, epsilonRotAxis) && aEqu)
+				|| rMe.multBy(-1).equalsEps(rOther, false, epsilonRotAxis) && aEquMinus){
+			if (clean && !equals(other))
+				set(other);
+			return true;
+		} else
+			return false;
+	}
+
+	// --- Private ---
+
+	/** set to identity & validRotAxis to true. */
+	private void reset() {
+		w = 1;
+		if (!xyz.equals(zero))
+			xyz.set(zero);
+		rotAxis = null;
+		angle = 0;
+		validRotAxis = true;
+	}
+	
+	/** Set WXYZ of the quaternion (set validRotAxis to false if not identity). Normalize WXYZ if normalize. */
+	private void setWXYZ(float w, Vector xyz, boolean normalize) {
+		// if identity
+		if ((w == -1 || w == 1)) {
+			reset();
+		} else {
+			this.w = w;
+			this.xyz.set(xyz);
+			if (normalize)
+				normalizeWXYZ();
+			this.validRotAxis = false;
+		}
+	}
+
+	/** Set the rotation axis and angle of the quaternion (set validRotAxis to true). Normalize rotAxis if normalize. */
+	private void setRotAxisAngle(Vector rotAxis, float angle, boolean normalize) {
+		if (rotAxis == null || (angle = validAngle(angle)) == 0) {
+			this.rotAxis = null;
+			this.angle = 0;
+		} else {
+			this.rotAxis = rotAxis.copy();
+			this.angle = angle;
+			// normalize rotAxis if needed
+			assert (!this.rotAxis.equals(zero));
+			if (normalize)
+				this.rotAxis.normalize();
+		}
+		this.validRotAxis = true;
+		testThis();
+	}
+	
+	/** Return an angle in radian in [-pi;pi[*/
+	private float validAngle(float angle) {
+		while (angle < 0)
+			angle+=twoPi;
+		return mod(angle+pi, twoPi)-pi;
+	}
+	
+	private boolean anglesEquEps(float a1, float a2) {
+		return Physic.equalsEps(a1, a2, epsilonAngle) || Physic.equalsEps(validAngle(max(a1, a2)+epsilonAngle)-epsilonAngle, min(a1, a2));
+	}
+	
+	/** Normalize the WXYZ components. */
+	private Quaternion normalizeWXYZ() {
+		final float normSq = normSq();
+		assert (!Physic.equalsEps(normSq, 0));
+		if (normSq != 1) {
+			final float norm = PApplet.sqrt(normSq);
+			final float invNorm = 1/norm;
+			setWXYZ(w * invNorm, xyz.multBy(invNorm), false);
+		}
+		return this;
+	}
+	
+	/** Check if the quat is a quaternion from w. return true if it was Identity (independently of update or not). */
+	private boolean checkIdentity() {
+		final boolean id = w == 1 || w == -1;
+		if (id) {
+			if (!validRotAxis) { // =~ not checked
+				reset();
+			}
+		} else {
+			// should be checked before setting the angles
+			assert (!validRotAxis);
+		}
+		return id;
+	}
+	
+	private float normSq() {
+		return w*w + xyz.magSq();
+	}
+}
