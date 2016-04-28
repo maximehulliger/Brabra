6b989ae312fcfca62819b44fd814f90eeffe8dab
Maxime Hulliger <maxime.hulliger@epfl.ch>
2016-04-28 04:14:32 +0200

    minor

--- src/brabra/gui/field/SceneField.java
+++ src/brabra/gui/field/SceneField.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 
 public class SceneField extends Field {
